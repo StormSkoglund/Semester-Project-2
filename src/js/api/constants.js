08b5b2ca768b359a94c7b19ed6769073d7ca0bd7
Alex Storm Skoglund <alesko52196@stud.noroff.no>
2024-05-02 19:49:35 +0200

    Fixes warnings for required inputs

--- src/js/api/constants.js
+++ src/js/api/constants.js
@@ -9,6 +9,7 @@ export const showError = document.querySelector(".errorContainer");
 export const username = document.getElementById("username");
 export const email = document.getElementById("email");
 export const avatar = document.getElementById("avatar");
+export const avatarAlt = document.getElementById("avatarAlt");
 // Containers
 export const showTryCatchError = document.querySelector(".tryCatchContainer");
 export const successContainer = document.querySelector(".successContainer");
